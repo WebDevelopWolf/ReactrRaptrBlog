54d3ce068d84ebc4e60c2ffe09d987d0bfa7a2cc
Liane Stevenson <liane.stevenson@honeygroup.co.uk>
2022-07-20 00:53:47 +0100

    Added masonry

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './header/header.component';
+import { NgxMasonryModule } from 'ngx-masonry';
 
 @NgModule({
-  declarations: [	
+  declarations: [
     AppComponent,
       HeaderComponent,
       HeaderComponent
    ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    NgxMasonryModule
   ],
   providers: [
     { provide: Window, useValue: window }
